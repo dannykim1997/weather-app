14a2e488164122a53c6b10d1c485fe4bc8aad7b6
Daniel Kim <78152949+dannykim1997@users.noreply.github.com>
2023-10-19 16:34:57 -0700

    Update readme and push app

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Weather-App
+
+Simple weather app with search functionality. 
+
+Utilizes data from weatherapi.com in order to display weather conditions based on city that user types in.
+
+dannykim1997.github.io/weather-app
--- app.js
+++ app.js
@@ -1,1 +1,56 @@
-console.log('js file connected');
+
+
+async function getWeather(city) {
+    try {
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=879d18d5838246089ec22422231410&q=${city}`, {mode: 'cors'});
+
+        if (!response.ok) {
+            throw new Error(`Weather data not found for ${city}`);
+        }
+
+        const responseData = await response.json();
+
+        const location = document.querySelector('.location');
+        const temp = document.querySelector('.temp');
+        const condition = document.querySelector('.condition');
+        const humidity = document.querySelector('.humidity');
+        const wind = document.querySelector('.wind');
+        const conditionImage = document.querySelector('.condition-image');
+        const iconUrl = `https:${responseData.current.condition.icon}`;
+        
+        location.textContent = `${responseData.location.name}, ${responseData.location.region}`;
+        temp.textContent = `${responseData.current.temp_f} F°`;
+        conditionImage.querySelector('img').src = `${iconUrl}`;
+        condition.textContent = `${responseData.current.condition.text}`;
+        humidity.textContent = `Humidity: ${responseData.current.humidity}`;
+        wind.textContent = `Wind: ${responseData.current.wind_mph} MPH`;
+    } 
+
+    catch (error) {
+        const location = document.querySelector('.location');
+        const temp = document.querySelector('.temp');
+        const condition = document.querySelector('.condition');
+        const humidity = document.querySelector('.humidity');
+        const wind = document.querySelector('.wind');
+        const conditionImage = document.querySelector('.condition-image');
+
+        location.textContent = `Error: ${error.message}`;
+        temp.textContent = '';
+        condition.textContent = '';
+        humidity.textContent = '';
+        wind.textContent = '';
+        conditionImage.querySelector('img').src = 'https://static.vecteezy.com/system/resources/previews/013/743/806/original/sad-face-emoji-png.png';
+    }
+}
+
+const cityForm = document.getElementById('city-form');
+
+cityForm.addEventListener('submit', function (e) {
+    e.preventDefault(); 
+
+    const cityInput = document.getElementById('city-input');
+    const city = cityInput.value;
+    getWeather(city);
+});
+
+getWeather('Los Angeles');
